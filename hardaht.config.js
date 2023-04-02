bd8794b2123b9c981a9ebbcdf76f86b568e061f4
Mark Feguson <129363712+polilpop@users.noreply.github.com>
2023-04-02 18:08:38 +0300

    Create hardaht.config.js

---
+++ hardaht.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/<key>",
+      accounts: [privateKey1, privateKey2, ...]
+    }
+  },
+  solidity: {
+    version: "0.5.15",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
